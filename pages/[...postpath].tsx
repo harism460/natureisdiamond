145a4c4eb273aeab122d1c2a01091e2e6113b431
CodeSpikey <codespikex@gmail.com>
2023-08-18 18:30:21 +0500

    Moved redirect to client side

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -1,28 +1,21 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import Head from 'next/head';
 import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
+const removeTags = (str: string) => {
+	if (str === null || str === '') return '';
+	else str = str.toString();
+	return str.replace(/(<([^>]+)>)/gi, '').replace(/\[[^\]]*\]/, '');
+};
+
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
 	const endpoint = process.env.GRAPHQL_ENDPOINT as string;
 	const graphQLClient = new GraphQLClient(endpoint);
-	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
 	const path = pathArr.join('/');
 	console.log(path);
-	const fbclid = ctx.query.fbclid;
 
-	// redirect if facebook is the referer or request contains fbclid
-	if (referringURL?.includes('facebook.com') || fbclid) {
-		return {
-			redirect: {
-				permanent: false,
-				destination: `${
-					endpoint.replace(/(\/graphql\/)/, '/') + encodeURI(path as string)
-				}`,
-			},
-		};
-	}
 	const query = gql`
 		{
 			post(id: "/${path}/", idType: URI) {
@@ -32,12 +25,6 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 				link
 				dateGmt
 				modifiedGmt
-				content
-				author {
-					node {
-						name
-					}
-				}
 				featuredImage {
 					node {
 						sourceUrl
@@ -54,58 +41,63 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			notFound: true,
 		};
 	}
+
 	return {
 		props: {
-			path,
-			post: data.post,
-			host: ctx.req.headers.host,
+			metaTags: {
+				title: data.post.title,
+				description: removeTags(data.post.excerpt).trim(),
+				url: data.post.link,
+				image: data.post.featuredImage.node.sourceUrl,
+				imageAlt: data.post.featuredImage.node.altText,
+				publishedTime: data.post.dateGmt,
+				modifiedTime: data.post.modifiedGmt,
+			} as MetaTags,
+			host: ctx.req.headers.host
 		},
 	};
 };
 
+interface MetaTags {
+	title: string
+	description: string
+	url: string
+	image: string
+	imageAlt?: string
+	publishedTime: string
+	modifiedTime: string
+}
+
 interface PostProps {
-	post: any;
-	host: string;
-	path: string;
+	metaTags: MetaTags
+	host: string
 }
 
 const Post: React.FC<PostProps> = (props) => {
-	const { post, host, path } = props;
+	const { metaTags, host } = props;
 
-	// to remove tags from excerpt
-	const removeTags = (str: string) => {
-		if (str === null || str === '') return '';
-		else str = str.toString();
-		return str.replace(/(<([^>]+)>)/gi, '').replace(/\[[^\]]*\]/, '');
-	};
+	useLayoutEffect(() => {
+		window.location.href = metaTags.url;
+	})
 
 	return (
 		<>
 			<Head>
-				<meta property="og:title" content={post.title} />
-				<link rel="canonical" href={`https://${host}/${path}`} />
-				<meta property="og:description" content={removeTags(post.excerpt)} />
-				<meta property="og:url" content={`https://${host}/${path}`} />
+				<meta property="og:title" content={metaTags.title} />
+				<meta property="og:description" content={metaTags.description} />
+				<meta property="og:url" content={metaTags.url} />
 				<meta property="og:type" content="article" />
 				<meta property="og:locale" content="en_US" />
 				<meta property="og:site_name" content={host.split('.')[0]} />
-				<meta property="article:published_time" content={post.dateGmt} />
-				<meta property="article:modified_time" content={post.modifiedGmt} />
-				<meta property="og:image" content={post.featuredImage.node.sourceUrl} />
+				<meta property="article:published_time" content={metaTags.publishedTime} />
+				<meta property="article:modified_time" content={metaTags.modifiedTime} />
+				<meta property="og:image" content={metaTags.image} />
 				<meta
 					property="og:image:alt"
-					content={post.featuredImage.node.altText || post.title}
+					content={metaTags.imageAlt || metaTags.title}
 				/>
-				<title>{post.title}</title>
+				<title>{metaTags.title}</title>
 			</Head>
-			<div className="post-container">
-				<h1>{post.title}</h1>
-				<img
-					src={post.featuredImage.node.sourceUrl}
-					alt={post.featuredImage.node.altText || post.title}
-				/>
-				<article dangerouslySetInnerHTML={{ __html: post.content }} />
-			</div>
 		</>
 	);
 };
